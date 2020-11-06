9375c8020b96a81075278e5d5f2126a25bcb340b
s2please <kimlam2207@gmail.com>
2020-11-07 02:50:32 +0700

    ....

--- S2Please/Areas/ADMIN/ViewModel/ProductSaveViewModel.cs
+++ S2Please/Areas/ADMIN/ViewModel/ProductSaveViewModel.cs
@@ -20,13 +20,6 @@ namespace S2Please.Areas.ADMIN.ViewModel
         public List<FileModel> FileColors { get; set; } = new List<FileModel>();
         public List<FileModel> FileSlides { get; set; } = new List<FileModel>();
 
-        public string ErrorListImg_1 { get; set; } = FunctionHelpers.GetValueLanguage("Product.Error.ListImage1");
-        public string ErrorListImg_2 { get; set; } = FunctionHelpers.GetValueLanguage("Product.Error.ListImage2");
-        public string ErrorListImg_3 { get; set; } = FunctionHelpers.GetValueLanguage("Product.Error.ListImage3");
-        public string ErrorListImg_4 { get; set; } = FunctionHelpers.GetValueLanguage("Product.Error.ListImage4");
-        public string ErrorVideo { get; set; } = FunctionHelpers.GetValueLanguage("Product.Error.Video");
-        public string ErrorVideoFormat { get; set; } = FunctionHelpers.GetValueLanguage("Product.Error.VideoFormat");
-
     }
 
 }
